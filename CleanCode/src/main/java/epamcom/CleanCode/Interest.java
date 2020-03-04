4f5a8c0419b42808007fd2db65d0c065725deb21
Anjani2812 <17wh1a0583@bvrithyderabad.edu.in>
2020-03-04 22:14:40 +0530

    Clean code and serialization

---
+++ CleanCode/src/main/java/epamcom/CleanCode/Interest.java
@@ -0,0 +1,13 @@
+package epamcom.CleanCode;
+
+
+public class Interest {
+	public static void main(String[] args) {
+		Construction.CostEstimation();
+		SimpleInterest.simpleInterestCalculator();
+		}
+
+}
+
+
+
